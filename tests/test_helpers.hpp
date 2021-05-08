79f8fc5146fdda275763ad194674e5a8eedecb9e
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-05-08 16:44:26 +0100

    Fix issue building on GCC due to GCC not supporting inline template specialization

--- tests/test_helpers.hpp
+++ tests/test_helpers.hpp
@@ -31,44 +31,12 @@ namespace com::saxbophone::tr_sort::PRIVATE::test_helpers {
             return this->_generate<T, T, std::uniform_int_distribution>(size);
         }
 
-        /*
-         * for 8-bit types, almost exactly the same, except generate as larger
-         * type due to no stdlib random number distribution accepting them as
-         * a type to generate
-         */
-
-        template <> std::vector<std::uint8_t> generate<std::uint8_t>(std::size_t size) {
-            return this->_generate<std::uint8_t, std::uint16_t, std::uniform_int_distribution>(size);
-        }
-
-        template <> std::vector<std::int8_t> generate<std::int8_t>(std::size_t size) {
-            return this->_generate<std::int8_t, std::int16_t, std::uniform_int_distribution>(size);
-        }
-
-        /*
-         * floating-point types are all very similar except that they use the
-         * floating-point number distribution
-         */
-
-        template <> std::vector<float> generate<float>(std::size_t size) {
-            return this->_generate<float, float, std::uniform_real_distribution>(size);
-        }
-
-        template <> std::vector<double> generate<double>(std::size_t size) {
-            return this->_generate<double, double, std::uniform_real_distribution>(size);
-        }
-
-        template <> std::vector<long double> generate<long double>(std::size_t size) {
-            // TODO: this really doesn't work well. Pick a better distribution for Reals
-            return this->_generate<long double, long double, std::uniform_real_distribution>(size);
-        }
-
     private:
         template <typename T, typename GenType, template<class> class RandomNumberDistribution>
         std::vector<T> _generate(std::size_t size) {
             // create the distribution
             RandomNumberDistribution<GenType> dist(
-                std::numeric_limits<T>::lowest(),
+                std::numeric_limits<T>::min(),
                 std::numeric_limits<T>::max()
             );
             // pre-allocate and initialise to size
@@ -80,6 +48,38 @@ namespace com::saxbophone::tr_sort::PRIVATE::test_helpers {
 
         std::default_random_engine engine;
     };
+
+    /*
+     * for 8-bit types, almost exactly the same, except generate as larger
+     * type due to no stdlib random number distribution accepting them as
+     * a type to generate
+     */
+
+    template <> std::vector<std::uint8_t> PRNG::generate<std::uint8_t>(std::size_t size) {
+        return this->_generate<std::uint8_t, std::uint16_t, std::uniform_int_distribution>(size);
+    }
+
+    template <> std::vector<std::int8_t> PRNG::generate<std::int8_t>(std::size_t size) {
+        return this->_generate<std::int8_t, std::int16_t, std::uniform_int_distribution>(size);
+    }
+
+    /*
+     * floating-point types are all very similar except that they use the
+     * floating-point number distribution
+     */
+
+    template <> std::vector<float> PRNG::generate<float>(std::size_t size) {
+        return this->_generate<float, float, std::uniform_real_distribution>(size);
+    }
+
+    template <> std::vector<double> PRNG::generate<double>(std::size_t size) {
+        return this->_generate<double, double, std::uniform_real_distribution>(size);
+    }
+
+    template <> std::vector<long double> PRNG::generate<long double>(std::size_t size) {
+        // TODO: this really doesn't work well. Pick a better distribution for Reals
+        return this->_generate<long double, long double, std::uniform_real_distribution>(size);
+    }
 
     PRNG prng;
 }
