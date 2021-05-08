04100538e07bed8e251d5251c92dd0059265adb3
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-05-08 17:05:31 +0100

    Wrote base parametrisable test case for tr_sort

--- tests/tr-sort.cpp
+++ tests/tr-sort.cpp
@@ -1,14 +1,27 @@
+#include <algorithm>
+#include <vector>
+
 #include <catch2/catch.hpp>
 
 #include <tr-sort.hpp>
 
 #include "test_helpers.hpp"
 
-using namespace com::saxbophone::tr_sort;
-using namespace com::saxbophone::tr_sort::PRIVATE::test_helpers;
 
-// dummy code
-TEST_CASE("Library works") {
-    std::vector<long double> data = prng.generate<long double>(1024);
-    REQUIRE(sort<long double>({data}));
+using namespace com::saxbophone;                                 // tr_sort
+using namespace com::saxbophone::tr_sort::PRIVATE::test_helpers; // prng
+
+TEST_CASE("Base test case for sort()") {
+    // PARAM: TYPE (int)
+    // PARAM: SIZE (100)
+    std::vector<int> unsorted_data = prng.generate<int>(100);
+    // create two copies to work-in-place
+    std::vector<int> tr_sorted_data = unsorted_data;
+    std::vector<int> stable_sorted_data = unsorted_data;
+    // sort with tr-sort and stdlib stable_sort
+    REQUIRE(tr_sort::sort<int>({tr_sorted_data}));
+    // stable_sort returns void so no need to test return value
+    std::stable_sort(stable_sorted_data.begin(), stable_sorted_data.end());
+    // verify both sorted arrays are equal
+    REQUIRE(tr_sorted_data == stable_sorted_data);
 }
