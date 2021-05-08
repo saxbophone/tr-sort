2ff00f7d86a266c838b01269e9a2d810e0e9199f
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-05-08 17:23:36 +0100

    Completed main parametrised test case of tr-sort Things not yet done: - testing with different number distributions - proper testing with floating point types (need extra work to get sensible number ranges on generated data)

--- tests/tr-sort.cpp
+++ tests/tr-sort.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <cstddef>
+#include <cstdint>
 #include <vector>
 
 #include <catch2/catch.hpp>
@@ -11,15 +13,20 @@
 using namespace com::saxbophone;                                 // tr_sort
 using namespace com::saxbophone::tr_sort::PRIVATE::test_helpers; // prng
 
-TEST_CASE("Base test case for sort()") {
-    // PARAM: TYPE (int)
+TEMPLATE_TEST_CASE(
+    "tr_sort::sort() does a stable sort on input arrays of varying size", "",
+    std::uint8_t, std::int8_t, std::uint16_t, std::int16_t, std::uint32_t,
+    std::int32_t, std::uint64_t, std::int64_t, float, double, long double
+) {
+    std::size_t test_exponent = GENERATE(range(2u, 4u));
+    std::size_t array_length = std::pow(10u, test_exponent);
     // PARAM: SIZE (100)
-    std::vector<int> unsorted_data = prng.generate<int>(100);
+    std::vector<TestType> unsorted_data = prng.generate<TestType>(array_length);
     // create two copies to work-in-place
-    std::vector<int> tr_sorted_data = unsorted_data;
-    std::vector<int> stable_sorted_data = unsorted_data;
+    std::vector<TestType> tr_sorted_data = unsorted_data;
+    std::vector<TestType> stable_sorted_data = unsorted_data;
     // sort with tr-sort and stdlib stable_sort
-    REQUIRE(tr_sort::sort<int>({tr_sorted_data}));
+    REQUIRE(tr_sort::sort<TestType>({tr_sorted_data}));
     // stable_sort returns void so no need to test return value
     std::stable_sort(stable_sorted_data.begin(), stable_sorted_data.end());
     // verify both sorted arrays are equal
