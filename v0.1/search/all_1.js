80f9c1659d777546becfdd5817f0c21439447d87
saxbophone <saxbophone@users.noreply.github.com>
2021-05-09 01:31:19 +0000

    Deploying to gh-pages from @ saxbophone/tr-sort@1b978630d7bd83257b7d3423b8a4d1baf8a4e232 🚀

---
+++ v0.1/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sort_1',['sort',['../namespacecom_1_1saxbophone_1_1tr__sort.html#ad49cba2e368c4f44b7b74b58a8d8349e',1,'com::saxbophone::tr_sort']]]
+];
---
+++ v0.1/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "cst",
+  1: "c",
+  2: "t",
+  3: "s",
+  4: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "files",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Files",
+  3: "Functions",
+  4: "Pages"
+};
+
