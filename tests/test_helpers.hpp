a2e3d33a6d67382adadd3563f6c5c7e9a21f57cd
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-05-09 01:57:52 +0100

    Improve thoroughness of unit tests Previously, they were failing inconsistently between OSes. Fixed this by adding non-consistent random generation and increasing the number of tests per array size

--- tests/test_helpers.hpp
+++ tests/test_helpers.hpp
@@ -11,20 +11,10 @@
 using namespace com::saxbophone::tr_sort;
 
 namespace com::saxbophone::tr_sort::PRIVATE::test_helpers {
-    // template<std::size_t SIZE>
-    // std::array<Byte, SIZE> generate_random_bytes() {
-    //     std::array<Byte, SIZE> data;
-    //     std::default_random_engine engine;
-    //     // N.B: Can't use uint8_t for type as generator doesn't support char types
-    //     std::uniform_int_distribution<std::uint16_t> prng(0x0000, 0x00FF);
-    //     for (auto& d : data) {
-    //         d = (Byte)prng(engine);
-    //     }
-    //     return data;
-    // }
-
     class PRNG {
     public:
+        PRNG() : engine(std::random_device()()) {}
+
         // default --assume integral types
         template <typename T>
         std::vector<T> generate(std::size_t size) {
--- tests/tr-sort.cpp
+++ tests/tr-sort.cpp
@@ -20,16 +20,18 @@ TEMPLATE_TEST_CASE(
 ) {
     std::size_t test_exponent = GENERATE(range(1u, 2u));
     std::size_t array_length = std::pow(10u, test_exponent);
-    // PARAM: SIZE (100)
-    std::vector<TestType> unsorted_data = prng.generate<TestType>(array_length);
-    // create two copies to work-in-place
-    std::vector<TestType> tr_sorted_data = unsorted_data;
-    std::vector<TestType> stable_sorted_data = unsorted_data;
-    // sort with tr-sort and stdlib stable_sort
-    REQUIRE(tr_sort::sort<TestType>({tr_sorted_data}));
-    // stable_sort returns void so no need to test return value
-    std::stable_sort(stable_sorted_data.begin(), stable_sorted_data.end());
-    // verify both sorted arrays are equal
-    // bool equal = tr_sorted_data == stable_sorted_data;
-    REQUIRE(tr_sorted_data == stable_sorted_data);
+    for (int i = 0; i < 1000000; i++) {
+        // PARAM: SIZE (100)
+        std::vector<TestType> unsorted_data = prng.generate<TestType>(array_length);
+        // create two copies to work-in-place
+        std::vector<TestType> tr_sorted_data = unsorted_data;
+        std::vector<TestType> stable_sorted_data = unsorted_data;
+        // sort with tr-sort and stdlib stable_sort
+        REQUIRE(tr_sort::sort<TestType>({tr_sorted_data}));
+        // stable_sort returns void so no need to test return value
+        std::stable_sort(stable_sorted_data.begin(), stable_sorted_data.end());
+        // verify both sorted arrays are equal
+        // bool equal = tr_sorted_data == stable_sorted_data;
+        REQUIRE(tr_sorted_data == stable_sorted_data);
+    }
 }
