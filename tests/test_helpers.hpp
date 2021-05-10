47cbab7f4b7e7ea9276abedf290f29ecb82b7f2f
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-05-10 12:35:15 +0100

    Initial bulk of work randomly choosing random number dists for tests TODO: Use generator-specific varied initialisation parameters for each generator

--- tests/test_helpers.hpp
+++ tests/test_helpers.hpp
@@ -7,6 +7,9 @@
 #include <random>
 #include <vector>
 
+#include <iostream>
+#include <typeinfo>
+
 
 using namespace com::saxbophone::tr_sort;
 
@@ -18,27 +21,95 @@ namespace com::saxbophone::tr_sort::PRIVATE::test_helpers {
         // default --assume integral types
         template <typename T>
         std::vector<T> generate(std::size_t size) {
-            return this->_generate<T, T, std::uniform_int_distribution>(size);
+            return this->_generate<
+                T,
+                T,
+                std::uniform_int_distribution,
+                std::binomial_distribution,
+                std::negative_binomial_distribution,
+                std::geometric_distribution,
+                std::poisson_distribution,
+                std::discrete_distribution
+            >(size);
         }
 
     private:
-        std::size_t _roll(std::size_t choices) {
-            std::uniform_int_distribution<std::size_t> dice(0, choices - 1);
-            return dice(this->_mengine);
-        }
+        // for randomly choosing one from a number of RandomNumberDistributions
+        template <
+            typename T,
+            typename GenType,
+            template<class> class... RandomNumberDistributions
+        >
+        class RND {
+        };
 
-        template <typename T, typename GenType, template<class> class RandomNumberDistribution>
+        template <
+            typename T,
+            typename GenType,
+            template<class> class RandomNumberDistribution,
+            template<class> class... RandomNumberDistributions
+        >
+        class RND<
+            T,
+            GenType,
+            RandomNumberDistribution,
+            RandomNumberDistributions...
+        > {
+        public:
+            static std::vector<T> generate(
+                std::default_random_engine& engine,
+                std::size_t size,
+                int chosen_rnd=-1
+            ) {
+                // only need to choose if chosen == -1
+                int chosen = chosen_rnd;
+                if (chosen == -1) {
+                    std::size_t rnds = sizeof...(RandomNumberDistributions) + 1;
+                    chosen = RND::_roll(engine, rnds);
+                }
+                // if it's 0, use this one, otherwise, go further down the chain
+                if (chosen == 0) {
+                    // create the distribution
+                    // RandomNumberDistribution<GenType> dist(
+                    //     std::numeric_limits<T>::min(),
+                    //     std::numeric_limits<T>::max()
+                    // );
+                    // std::cout << typeid(RandomNumberDistribution<GenType>).name() << std::endl;
+                    RandomNumberDistribution<GenType> dist;
+                    // pre-allocate and initialise to size
+                    std::vector<T> data(size);
+                    // fill vector with lambda calling prng
+                    std::generate(data.begin(), data.end(), [&](){ return (T)dist(engine); });
+                    return data;
+                } else {
+                    return RND<T, GenType, RandomNumberDistributions...>::generate(engine, size, chosen - 1);
+                }
+            }
+        private:
+            static std::size_t _roll(std::default_random_engine& engine, std::size_t choices) {
+                std::uniform_int_distribution<std::size_t> dice(0, choices - 1);
+                return dice(engine);
+            }
+        };
+
+        template <
+            typename T,
+            typename GenType
+        >
+        class RND<T, GenType> {
+        public:
+            static std::vector<T> generate(
+                std::default_random_engine&,
+                std::size_t,
+                int=-1
+            ) {
+                return {};
+            }
+        };
+
+        template <typename T, typename GenType, template<class> class... RandomNumberDistributions>
         std::vector<T> _generate(std::size_t size) {
-            // create the distribution
-            RandomNumberDistribution<GenType> dist(
-                std::numeric_limits<T>::min(),
-                std::numeric_limits<T>::max()
-            );
-            // pre-allocate and initialise to size
-            std::vector<T> data(size);
-            // fill vector with lambda calling prng
-            std::generate(data.begin(), data.end(), [&](){ return (T)dist(this->_engine); });
-            return data;
+            return RND<T, GenType, RandomNumberDistributions...>::generate(this->_engine, size);
         }
 
         std::default_random_engine _engine;
@@ -51,11 +122,29 @@ namespace com::saxbophone::tr_sort::PRIVATE::test_helpers {
      */
 
     template <> std::vector<std::uint8_t> PRNG::generate<std::uint8_t>(std::size_t size) {
-        return this->_generate<std::uint8_t, std::uint16_t, std::uniform_int_distribution>(size);
+        return this->_generate<
+            std::uint8_t,
+            std::uint16_t,
+            std::uniform_int_distribution,
+            std::binomial_distribution,
+            std::negative_binomial_distribution,
+            std::geometric_distribution,
+            std::poisson_distribution,
+            std::discrete_distribution
+        >(size);
     }
 
     template <> std::vector<std::int8_t> PRNG::generate<std::int8_t>(std::size_t size) {
-        return this->_generate<std::int8_t, std::int16_t, std::uniform_int_distribution>(size);
+        return this->_generate<
+            std::int8_t,
+            std::int16_t,
+            std::uniform_int_distribution,
+            std::binomial_distribution,
+            std::negative_binomial_distribution,
+            std::geometric_distribution,
+            std::poisson_distribution,
+            std::discrete_distribution
+        >(size);
     }
 
     /*
@@ -64,16 +153,64 @@ namespace com::saxbophone::tr_sort::PRIVATE::test_helpers {
      */
 
     template <> std::vector<float> PRNG::generate<float>(std::size_t size) {
-        return this->_generate<float, float, std::uniform_real_distribution>(size);
+        return this->_generate<
+            float,
+            float,
+            std::uniform_real_distribution,
+            std::exponential_distribution,
+            std::gamma_distribution,
+            std::weibull_distribution,
+            std::extreme_value_distribution,
+            std::normal_distribution,
+            std::lognormal_distribution,
+            std::chi_squared_distribution,
+            std::cauchy_distribution,
+            std::fisher_f_distribution,
+            std::student_t_distribution,
+            std::piecewise_constant_distribution,
+            std::piecewise_linear_distribution
+        >(size);
     }
 
     template <> std::vector<double> PRNG::generate<double>(std::size_t size) {
-        return this->_generate<double, double, std::uniform_real_distribution>(size);
+        return this->_generate<
+            double,
+            double,
+            std::uniform_real_distribution,
+            std::exponential_distribution,
+            std::gamma_distribution,
+            std::weibull_distribution,
+            std::extreme_value_distribution,
+            std::normal_distribution,
+            std::lognormal_distribution,
+            std::chi_squared_distribution,
+            std::cauchy_distribution,
+            std::fisher_f_distribution,
+            std::student_t_distribution,
+            std::piecewise_constant_distribution,
+            std::piecewise_linear_distribution
+        >(size);
     }
 
     template <> std::vector<long double> PRNG::generate<long double>(std::size_t size) {
         // TODO: this really doesn't work well. Pick a better distribution for Reals
-        return this->_generate<long double, long double, std::uniform_real_distribution>(size);
+        return this->_generate<
+            long double,
+            long double,
+            std::uniform_real_distribution,
+            std::exponential_distribution,
+            std::gamma_distribution,
+            std::weibull_distribution,
+            std::extreme_value_distribution,
+            std::normal_distribution,
+            std::lognormal_distribution,
+            std::chi_squared_distribution,
+            std::cauchy_distribution,
+            std::fisher_f_distribution,
+            std::student_t_distribution,
+            std::piecewise_constant_distribution,
+            std::piecewise_linear_distribution
+        >(size);
     }
 
     PRNG prng;
