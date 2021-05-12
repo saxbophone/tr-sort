76d303a61093568cce45f6746ec2b00bf502c285
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-05-12 10:00:32 +0100

    Add new failing test cases for sorting float input containing extreme values and infinities

--- tests/tr-sort.cpp
+++ tests/tr-sort.cpp
@@ -20,19 +20,51 @@ TEMPLATE_TEST_CASE(
 ) {
     std::size_t test_exponent = GENERATE(range(1u, 4u));
     std::size_t array_length = (std::size_t)std::pow(10u, test_exponent);
-    for (int i = 0; i < 1000; i++) {
-        // PARAM: SIZE (100)
-        std::vector<TestType> unsorted_data = prng.generate<TestType>(array_length);
-        // create two copies to work-in-place
-        std::vector<TestType> tr_sorted_data = unsorted_data;
-        std::vector<TestType> stable_sorted_data = unsorted_data;
-        // sort with tr-sort and stdlib stable_sort
-        REQUIRE(tr_sort::sort<TestType>({tr_sorted_data}));
-        // stable_sort returns void so no need to test return value
-        std::stable_sort(stable_sorted_data.begin(), stable_sorted_data.end());
-        // verify both sorted arrays are equal
-        // bool equal = tr_sorted_data == stable_sorted_data;
-        REQUIRE(tr_sorted_data == stable_sorted_data);
-        // REQUIRE(equal);
-    }
+    int test_iteration = GENERATE(1, 1000);
+    // PARAM: SIZE (100)
+    std::vector<TestType> unsorted_data = prng.generate<TestType>(array_length);
+    // create two copies to work-in-place
+    std::vector<TestType> tr_sorted_data = unsorted_data;
+    std::vector<TestType> stable_sorted_data = unsorted_data;
+    // sort with tr-sort and stdlib stable_sort
+    REQUIRE(tr_sort::sort<TestType>({tr_sorted_data}));
+    // stable_sort returns void so no need to test return value
+    std::stable_sort(stable_sorted_data.begin(), stable_sorted_data.end());
+    // verify both sorted arrays are equal
+    // bool equal = tr_sorted_data == stable_sorted_data;
+    REQUIRE(tr_sorted_data == stable_sorted_data);
+    // REQUIRE(equal);
+}
+
+TEMPLATE_TEST_CASE(
+    "tr_sort::sort() does a stable sort on input arrays containing only extreme finite values and infinities", "",
+    float, double, long double
+) {
+    const TestType VALUES[] = {
+        // finite:
+        std::numeric_limits<TestType>::lowest(),
+        std::numeric_limits<TestType>::max(),
+        // infinite:
+        -std::numeric_limits<TestType>::infinity(),
+        +std::numeric_limits<TestType>::infinity()
+    };
+    std::size_t array_size = 10;
+    std::default_random_engine engine;
+    std::uniform_int_distribution<std::size_t> picker(0, 3);
+    int test_iteration = GENERATE(1, 1000);
+    // generate random data
+    std::vector<TestType> unsorted_data(array_size);
+    std::generate(
+        unsorted_data.begin(),
+        unsorted_data.end(),
+        [&](){ return VALUES[picker(engine)]; }
+    );
+    std::vector<TestType> tr_sorted_data = unsorted_data;
+    std::vector<TestType> stable_sorted_data = unsorted_data;
+    // sort with tr-sort and stdlib stable_sort
+    REQUIRE(tr_sort::sort<TestType>({tr_sorted_data}));
+    // stable_sort returns void so no need to test return value
+    std::stable_sort(stable_sorted_data.begin(), stable_sorted_data.end());
+    // results should be the same
+    REQUIRE(tr_sorted_data == stable_sorted_data);
 }
