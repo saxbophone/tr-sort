fd110b98aae9f773e38a29574ca856b662e83f5c
saxbophone <saxbophone@users.noreply.github.com>
2022-04-03 06:52:30 +0000

    Deploying to gh-pages from @ saxbophone/tr-sort@6806b2c37b0ba2345eb61e9afc4f8151cf53c619 🚀

---
+++ v0.2/search/all_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['todo_20list_2',['Todo List',['../todo.html',1,'']]],
+  ['tr_2dsort_2ehpp_3',['tr-sort.hpp',['../tr-sort_8hpp.html',1,'']]],
+  ['transpose_20sort_4',['Transpose Sort',['../index.html',1,'']]]
+];
